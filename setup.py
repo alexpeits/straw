d182268fa445788f3e20b39fea4407aae11aa1be
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2016-08-19 16:11:17 +0300

    added setup.py and setup.cfg, removed examples.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='straw',
+    version=0.1,
+    author='Alex Peitsinis',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
